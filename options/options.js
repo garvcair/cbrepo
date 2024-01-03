ee36e5a89557c4a2eb8f29ba1c9e80a505108c18
Crowdbotics <team@crowdbotics.com>
2024-01-03 20:16:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-bush-45263",
+  url: "https://lively-bush-45263.botics.co",
+  api: "https://lively-bush-45263.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
